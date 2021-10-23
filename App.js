b025bde5d75d28fbbd520d92b3a9074a5ec47141
GRANJAW7 <granjaw7@macbook-pro-de-granjaw7.home>
2021-10-23 21:09:31 +0200

    finishing the project

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {View, StyleSheet, Text, Image, ScrollView} from 'react-native';
 const App = () => {
   return (
     <>
-      <View>
+      <ScrollView>
         <View style={{flexDirection: 'row'}}>
           <Image style={styles.banner} source={require('./assets/img/bg.jpg')} />
         </View>
@@ -28,8 +28,35 @@ const App = () => {
               <Image style={styles.ciudad} source={require('./assets/img/actividad5.jpg')} />
             </View>
           </ScrollView>
+          <Text style={styles.titulo}>Los Mejores Alojamientos</Text>
+          <View>
+            <View>
+              <Image style={styles.mejores} source={require('./assets/img/mejores1.jpg')} />
+            </View>
+            <View>
+              <Image style={styles.mejores} source={require('./assets/img/mejores2.jpg')} />
+            </View>
+            <View>
+              <Image style={styles.mejores} source={require('./assets/img/mejores3.jpg')} />
+            </View>
+          </View>
+          <Text style={styles.titulo}>Hospedajes en LA</Text>
+          <View style={styles.listado}>
+            <View style={styles.listadoItem}>
+              <Image style={styles.mejores} source={require('./assets/img/hospedaje1.jpg')} />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image style={styles.mejores} source={require('./assets/img/hospedaje2.jpg')} />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image style={styles.mejores} source={require('./assets/img/hospedaje3.jpg')} />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image style={styles.mejores} source={require('./assets/img/hospedaje4.jpg')} />
+            </View>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </>
   );
 };
@@ -48,6 +75,19 @@ const styles = StyleSheet.create({
     width: 250,
     height: 300,
     marginRight: 10,
+  },
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5,
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  listadoItem: {
+    flexBasis: '49%'
   },
   contenedor: {
     marginHorizontal: 10
